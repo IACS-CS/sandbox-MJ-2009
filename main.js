c17a95e36ff0f65b984e13b7a023d41511045544
MJ-2009 <januskiewiczmason60@gmail.com>
2025-09-03 16:25:59 +0000

    added some lines/played with program

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ let ti = new TextInterface(app, "Example Text Interface Program");
 
 ti.output("Welcome to my program!");
 let name = await ti.prompt("What is your name?");
-ti.output("Hello, " + name);
-
-
+if {mason}
+ti.output ("you suck balls");
+else ti.output
+ti.output("Hello, " + name );
+let color = await ti.prompt("what is your favorite color");
+ti.output("oh " + color + " is cool");
