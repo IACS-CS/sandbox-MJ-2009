ebe3e25cf2a53a14f88b47fe16580001d076a0a6
MJ-2009 <januskiewiczmason60@gmail.com>
2025-09-04 18:41:05 +0000

    its now stupid

--- main.js
+++ main.js
@@ -7,9 +7,16 @@ let ti = new TextInterface(app, "Example Text Interface Program");
 
 ti.output("Welcome to my program!");
 let name = await ti.prompt("What is your name?");
-if {mason}
-ti.output ("you suck balls");
-else ti.output
-ti.output("Hello, " + name );
+if (name==="mason") {
+  ti.output("you are the goat I love you :)")
+} else ti.output("you suck balls " + name);
 let color = await ti.prompt("what is your favorite color");
-ti.output("oh " + color + " is cool");
+if (color === "pink") { 
+  ti.output("eww " + name + " you are a weirdo");
+} else ti.output("oh " + color + " is cool");
+ti.output ("what do you want to do with your life?");
+let life = await ti.prompt("what do you want to do with your life?");
+if (life === "be a coder like mason") {
+  ti.output("good choice " + name + " you will be great");
+} else ti.output("oh " + life + " sucks you should follow in masons footsteps");  
+ti.output("Goodbye my friend " + name + " I love you ish ");
